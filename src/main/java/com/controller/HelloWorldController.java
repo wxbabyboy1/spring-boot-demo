74df93d1c82199e54d51a1eb27b09069dc446bbe
guolong.zha <guolong.zha@mtime.com>
2018-02-28 09:52:21 +0800

    简单启动

---
+++ src/main/java/com/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ZhaGuolong on 2018/2/28.
+ */
+
+@RestController
+@RequestMapping("/springboot")
+public class HelloWorldController {
+
+    @RequestMapping(value = "/say", method = RequestMethod.GET)
+    public String sayWorld(String name) {
+        return "Hello " + name;
+    }
+}
