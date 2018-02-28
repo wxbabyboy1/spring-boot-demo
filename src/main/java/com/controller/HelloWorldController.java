886d48c4a489e3fadbcfa90f5f46e74cfca1f776
guolong.zha <guolong.zha@mtime.com>
2018-02-28 12:28:05 +0800

    自定义参数、自定义参数引用

--- src/main/java/com/controller/HelloWorldController.java
+++ src/main/java/com/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/springboot")
 public class HelloWorldController {
+    @Value("${author.realname}")
+    private String realname;
+
+    @Value("${author.nickname}")
+    private String nickname;
+
+    @Value("${author.intro}")
+    private String intro;
 
     @RequestMapping(value = "/say", method = RequestMethod.GET)
     public String sayWorld(String name) {
-        return "Hello " + name;
+//        return "Hello " + name + " realName:" + realname + ", nickName:" + nickname;
+        return "Hello " + name + " intro:" + intro;
     }
 }
